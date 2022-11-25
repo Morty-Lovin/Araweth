e28e094fa03c7f133ecad6396401865d2ca8a925
Morty-Lovin <114520076+Morty-Lovin@users.noreply.github.com>
2022-11-25 12:45:43 -0600

    Add files via upload

---
+++ MortonJ_Lab07/main.cpp
@@ -0,0 +1,19 @@
+//Johnathen Morton
+//ITSE 1307-1022
+//Intro to C++
+//Lab 07
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	string * ptr[200];
+	char input;
+	
+	do {
+
+	} while (input = 'Y', 'y');
+
+}
